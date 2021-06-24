b62abd50d63e9107916a18a74ae28be817542693
Bhaskar Paul <paulbhaskar007@gmail.com>
2021-06-24 21:40:03 +0530

    minor changes

--- modules/functional.js
+++ modules/functional.js
@@ -1,16 +1,16 @@
 module.exports = {
-  replaceTemplate: (template, item) => {
-    let output = template.replaceAll("{%PRODUCT_NAME%}", item.productName);
-    output = output.replaceAll("{%IMAGE%}", item.image);
-    output = output.replaceAll("{%QUANTITY%}", item.quantity);
-    output = output.replaceAll("{%PRICE%}", item.price);
-    output = output.replaceAll("{%ID%}", item.id);
-    output = output.replaceAll("{%FROM_WHERE%}", item.from);
-    output = output.replaceAll("{%NUTRIENTS%}", item.nutrients);
-    output = output.replaceAll("{%DESCRIPTION%}", item.description);
-    if (!item.organic) {
-      output = output.replaceAll("{%NOT_ORGANIC%}", "not-organic");
-    }
-    return output;
-  },
+    replaceTemplate: (template, item) => {
+        let output = template.replaceAll("{%PRODUCT_NAME%}", item.productName);
+        output = output.replaceAll("{%IMAGE%}", item.image);
+        output = output.replaceAll("{%QUANTITY%}", item.quantity);
+        output = output.replaceAll("{%PRICE%}", item.price);
+        output = output.replaceAll("{%ID%}", item.id);
+        output = output.replaceAll("{%FROM_WHERE%}", item.from);
+        output = output.replaceAll("{%NUTRIENTS%}", item.nutrients);
+        output = output.replaceAll("{%DESCRIPTION%}", item.description);
+        if (!item.organic) {
+            output = output.replaceAll("{%NOT_ORGANIC%}", "not-organic");
+        }
+        return output;
+    },
 };
